a59c5ace3f2375e289d652ab22aed7bbe1e62ccc
ViES <>
2024-10-10 00:52:45 +0100

    fix music

--- src/NyancatMusic.tsx
+++ src/NyancatMusic.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NyancatMusic: React.FC = () => {
-  return <audio src="/nyancat.ogg" loop autoPlay preload="auto"/>;
+  return <audio src={`${process.env.PUBLIC_URL}/nyancat.ogg`} loop autoPlay preload="auto"/>;
 };
 
 export default NyancatMusic;
