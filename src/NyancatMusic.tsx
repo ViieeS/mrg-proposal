3eb000aedbf27abd0b6872dd2032abe81adf4ac8
vsuntsov <vsuntsov@nvidia.com>
2024-10-12 16:54:13 +0400

    fix audio for iOS

--- src/NyancatMusic.tsx
+++ src/NyancatMusic.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PUBLIC_URL } from './constatns';
 
 const NyancatMusic: React.FC = () => {
-  return <audio src={`${PUBLIC_URL}/nyancat.ogg`} loop autoPlay preload="auto"/>;
+  return <audio src={`${PUBLIC_URL}/nyancat.mp3`} loop autoPlay preload="auto"/>;
 };
 
 export default NyancatMusic;
