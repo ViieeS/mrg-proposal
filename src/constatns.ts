92d9362185b07f042f75d3908cf8c4f914de6ab3
vsuntsov <vsuntsov@nvidia.com>
2024-10-14 18:17:17 +0400

    adjust delays

--- src/constatns.ts
+++ src/constatns.ts
@@ -1,8 +1,8 @@
 export const PUBLIC_URL = "/mrg-proposal";
 
 export const INTRO_DELAY_MS = 100;
-export const PROPOSAL_DELAY_MS = 10000;
+export const PROPOSAL_DELAY_MS = 12000;
 
 // Starts counting after proposal typewriter is finished
 export const OUTCOMES_DELAY_MS = 2000;
-export const ANSWER_DELAY_MS = 10000;
+export const ANSWER_DELAY_MS = 12000;
