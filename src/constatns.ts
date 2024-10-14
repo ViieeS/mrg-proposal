dffde1f1f63840f1dfe1f20c506ce2eed562c0e2
vsuntsov <vsuntsov@nvidia.com>
2024-10-14 18:10:09 +0400

    adjust delays

--- src/constatns.ts
+++ src/constatns.ts
@@ -5,4 +5,4 @@ export const PROPOSAL_DELAY_MS = 10000;
 
 // Starts counting after proposal typewriter is finished
 export const OUTCOMES_DELAY_MS = 2000;
-export const ANSWER_DELAY_MS = 6000;
+export const ANSWER_DELAY_MS = 10000;
